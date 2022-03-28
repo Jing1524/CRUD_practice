a505c438d6dad7530c87a7a3da470d78c7ea210c
Jing Li <jingli1524@gmail.com>
2022-03-27 22:18:44 -0400

    folder structur/main app layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between">
+            <Grid item xs={12} sm={7}>
+              <Typography>Place holder for posts component</Typography>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Typography>Place holder for form component</Typography>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
